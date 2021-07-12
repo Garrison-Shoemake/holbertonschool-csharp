6b1faa14098632c7deb0f085f226cea3918094d5
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-12 22:43:45 +0000

    add task 6 files

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		Console.WriteLine("{0}{0}{0}", str);
+        string newstr = str.Substring(0, 9);
+        Console.WriteLine(newstr);
+    }      
+}
