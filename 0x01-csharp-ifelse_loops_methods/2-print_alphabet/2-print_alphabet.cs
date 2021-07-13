3087f39be9c322bc9af8274cf9ae8d5f48a0673e
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-13 22:34:40 +0000

    add task 2 files

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 97;
+            while (i < 123)
+            {
+                Console.Write((char)i);
+                i++;
+            }
+        }
+    }
+}
