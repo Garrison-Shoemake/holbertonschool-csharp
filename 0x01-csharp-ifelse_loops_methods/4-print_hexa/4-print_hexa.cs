d5fd176be948df8a3e0099f1508e18c2829dc995
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-13 22:59:31 +0000

    add task 4 files

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 99)
+            {
+                Console.WriteLine("{0} = 0x{0:x}", i);
+                i++;
+            }
+        }
+    }
+}
