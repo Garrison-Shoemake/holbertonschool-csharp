b7a5ca04e7459a17758da062cda7f729f238221f
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-13 22:43:26 +0000

    add task 3 files

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 97;
+            while (i < 123)
+            {
+                if (i != 101 && i != 113){
+                    Console.Write((char)i);
+                }
+                i++;
+            }
+        }
+    }
+}
