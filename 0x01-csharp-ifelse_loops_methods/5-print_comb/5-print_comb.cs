541874075dcc07fd9df150cf1da1792a933c0d35
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-13 23:07:59 +0000

    add task 5 files

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 100)
+            {                
+                if (i < 99)
+                {
+                    Console.Write("{0:D2}, ", i);
+                }
+                else 
+                {
+                    Console.WriteLine("{0:D2}", i);
+                }            
+                i++;
+            }
+        }
+    }
+}
