af262892ad93a5d19c00689bd2a079c742733f74
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-14 22:04:14 +0000

    add null check to top of file

--- 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -6,15 +6,15 @@ class Array
     public static void Reverse(int[] array)
     {
         int index;
+        if (array == null)
+        {
+            System.Console.WriteLine();
+        }
         for (index = array.Length - 1; index >= 0; index--)
         {
             if (index == 0)
             {
                 System.Console.Write("{0}", array[index]);
-            }
-            else if (array == null)
-            {
-                System.Console.WriteLine();
             }
             else
             {
