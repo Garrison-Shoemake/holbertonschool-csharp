1dc3ee04157d77946c2ef2ff1f0ab38dd2d77686
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-14 02:35:43 +0000

    add task 8 files

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int last = number % 10;
+        if (last < 0)
+        {
+            last = last * -1;
+        }
+        Console.Write(last);    
+        return last;
+    }
+
+}
+
