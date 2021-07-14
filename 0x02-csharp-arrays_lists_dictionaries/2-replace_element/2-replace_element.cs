f87752c728c6592913ec39bf808f48a866183000
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-14 21:23:03 +0000

    add files for task 2

---
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+class Array
+{
+    public static int[] ReplaceElement(int[] array, int index, int n)
+    {
+        if (index > array.Length - 1)
+        {
+            System.Console.WriteLine("Index out of range");
+            return array;
+        }
+        else if (index < 0)
+        {
+            System.Console.WriteLine("Index out of range");
+            return array;
+
+        }
+        else
+        {
+            array[index] = n;
+            return array;
+        }
+    }
+}
+
