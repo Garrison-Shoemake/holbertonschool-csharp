531013c951e2ca464b133c7359b7bcb105b77116
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-14 03:12:29 +0000

    add task 10 files

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        int i = 0;
+        if (length < 0)
+        {
+            Console.Write("");
+        }
+        while (i < length)
+        {
+            Console.Write("_");
+            i++;
+        }
+        Console.Write("\n");
+    }
+}
+
