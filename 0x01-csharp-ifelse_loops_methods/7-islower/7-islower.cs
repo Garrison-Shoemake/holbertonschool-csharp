72bd02303b672752d4660c38c44fc1b8d78dac68
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-14 02:23:54 +0000

    add files for task 7

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        if ((int)c > 96 && (int)c < 123)
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+}
