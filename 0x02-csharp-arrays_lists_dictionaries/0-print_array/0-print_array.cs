5ce3cd0a045c5ec6ba0d2275ed67dec1c430969d
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-14 20:02:42 +0000

    add files for task 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+//        if (size == 0)
+//        {
+//            Console.Write("");
+//        }
+        if (size < 0)
+        {
+            System.Console.Write("Size cannot be negative");
+        }
+        int[] array = new int[size];
+        for (int pos = 0; pos < size; pos++)
+        {
+           Console.Write("{0} ", pos);
+        }
+        System.Console.WriteLine();
+        return array;
+    }
+}
