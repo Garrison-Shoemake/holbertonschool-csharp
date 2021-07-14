b1ddbe87c6feba3e98cd786159670e2e746d4bf5
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-14 22:28:30 +0000

    add files for task 4

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            System.Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        var numbers = new List<int> {};
+        int index;
+
+        for (index = 0; index < size; index++)
+        {
+            numbers.Add(index);
+            System.Console.Write("{0}", numbers[index]);
+            if (index < size - 1)
+            {
+                System.Console.Write(" ");
+            }
+        }
+        System.Console.WriteLine();
+        return numbers;
+    }
+}
