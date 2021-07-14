8db6a8459b9c28ba98f24ea0ef25d799f9057614
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-14 21:02:58 +0000

    add files for task 1

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+        if (index > array.Length)
+        {
+            System.Console.WriteLine("Index out of range");
+            return -1;
+        }
+        if (index < 0)
+        {
+            System.Console.WriteLine("Index out of range");
+            return -1;
+        }
+        return array[index];
+    }
+}
+
