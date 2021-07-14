bb2bb559984fd3a5d564df8d6f2133427845ddd7
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-14 21:57:40 +0000

    add task 3 files

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        int index;
+        for (index = array.Length - 1; index >= 0; index--)
+        {
+            if (index == 0)
+            {
+                System.Console.Write("{0}", array[index]);
+            }
+            else
+            {
+                System.Console.Write("{0} ", array[index]);
+            }
+        }
+    }
+}
+
