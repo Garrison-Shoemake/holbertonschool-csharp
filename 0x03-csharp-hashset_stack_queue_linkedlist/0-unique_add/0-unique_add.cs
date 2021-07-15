c945b188078555afcf17ffe07def733f7f4b4169
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-15 18:37:16 +0000

    add task 0 files

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static int Sum(List<int> myList)
+        {
+            int sum = 0;
+            HashSet<int> adding = new HashSet<int>(myList);
+
+            foreach (int number in adding)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+    }
+
