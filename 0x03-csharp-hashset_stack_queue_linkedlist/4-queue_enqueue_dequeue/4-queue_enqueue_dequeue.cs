b028a8d4e4d7975d7398aeba04fc5d2c7821c88b
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-15 21:41:16 +0000

    add task 4 files

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        int count = 0;
+        foreach (string value in aQueue)
+        {
+            count++;
+        }
+        System.Console.WriteLine("Number of items: {0}", count);
+        if (aQueue.Count == 0)
+        {
+            System.Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            System.Console.WriteLine("Top item: {0}", aQueue.Peek());
+        }
+        System.Console.WriteLine("Queue contains \"{0}\": {1}", search, aQueue.Contains(search));
+        while (aQueue.Contains(search) == true)
+        {
+            aQueue.Dequeue();
+        }
+        aQueue.Enqueue(newItem);
+        return aQueue;
+    }
+}
