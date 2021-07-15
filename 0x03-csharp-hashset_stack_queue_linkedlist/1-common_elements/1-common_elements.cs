ed63db1c789f57fd78a69af6636bc3a7d17d552b
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-15 18:50:05 +0000

    add task 1 files

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> first = new HashSet<int>(list1);
+        HashSet<int> second = new HashSet<int>(list2);
+        first.IntersectWith(second);
+        List<int> retList = new List<int>(first);
+        return retList;
+    }
+}
+
