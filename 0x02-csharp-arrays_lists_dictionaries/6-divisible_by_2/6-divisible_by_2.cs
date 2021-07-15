b46a305417e3dde731b026cc53c55e54208ceb53
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-15 00:39:15 +0000

    add task 6 files

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> bools = new List<bool>();
+        foreach (int number in myList)
+        {
+            if (number % 2 == 0)
+            {
+                bools.Add(true);
+            }
+            else
+            {
+                bools.Add(false);
+            }
+        }
+        return bools;
+    }
+}
+
