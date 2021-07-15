a4187a14a007c8b54040087813fbf9835414dd61
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-15 02:35:03 +0000

    add task 13 files

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList == null)
+        {
+            return "None";
+        }
+        var max = myList.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+        return max;
+
+    }
+}
+
