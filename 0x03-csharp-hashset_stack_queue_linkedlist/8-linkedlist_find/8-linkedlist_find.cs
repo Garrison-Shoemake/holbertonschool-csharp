9497da8d5e0e286eb32536aef50618fc83baa90c
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-15 23:28:08 +0000

    add task 8 files

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int count = 0;
+        if (myLList.Contains(value) == false)
+        {
+            return -1;
+        }
+        foreach (int thing in myLList)
+        {
+            if (thing == value)
+            {
+                return count;
+            }
+            count++;
+        }
+        return count;
+    }
+}
+
