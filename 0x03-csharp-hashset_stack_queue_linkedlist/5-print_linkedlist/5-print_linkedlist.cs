f06f033cd90fe28f72582c0c643da864222ac16b
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-15 22:16:03 +0000

    add task 5 files

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> llist = new LinkedList<int>();
+        int i = 0;
+
+        while (i < size)
+        {
+            llist.AddFirst(i);
+            System.Console.WriteLine(i);
+            i++;
+        }
+
+        return llist;
+    }
+}
+
