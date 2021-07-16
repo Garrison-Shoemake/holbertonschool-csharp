56e6f9c8b421ab0e36c697abb1eefe794bd1f238
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-16 03:41:41 +0000

    add task 13 files

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        myLList.Remove(myLList.ElementAt(index));
+    }
+}
+
