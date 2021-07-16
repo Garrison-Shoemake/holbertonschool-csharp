2161924266f6036394069a2d9e62be17a56fd0da
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-16 01:39:04 +0000

    add files for tak 9

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        int ret = 0;
+        LinkedListNode<int> head = myLList.First;
+        ret = head.Value;
+        return ret;
+    }
+}
+
