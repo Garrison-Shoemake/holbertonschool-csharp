4c14e0619f132f0c31f481eb691850f6d460f348
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-16 02:00:21 +0000

    add task 10 files

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        try
+        {
+            return myLList.ElementAt(n);
+        }
+        catch
+        {
+            return 0;
+        }
+    }
+}
+
