3920a055a9c659c60591e779cd665e4691ad5c94
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-16 03:29:52 +0000

    add task 12 files

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> current = myLList.First;
+        LinkedListNode<int> newnode = new LinkedListNode<int>(n);
+        if (current == null)
+        {
+            myLList.AddFirst(newnode);
+            return newnode;
+        }
+        if (current.Value >=n)
+        {
+            myLList.AddBefore(current, newnode);
+            return newnode;
+        }
+        while (current.Next != null && current.Next.Value < n)
+        {
+            current = current.Next;
+        }
+        myLList.AddAfter(current, newnode);
+        return newnode;
+    }
+}
