2755a2a3f7b16f4f793710a5ddfb78d75f9579ba
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-21 22:18:59 +0000

    add task 2 files

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Represents Zombie class</summary>
+    public class Zombie
+    {
+        ///<summary>Sets health value</summary>
+        public int health;
+        ///<summary>Sets heal to zero</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        ///<summary>Sets health above zero</summary>
+        public Zombie(int value)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            this.health = value;
+        }
+    }
+}
