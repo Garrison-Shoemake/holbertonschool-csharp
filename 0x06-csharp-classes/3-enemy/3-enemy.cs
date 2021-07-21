4ea5191493e9a99ced2d089934933d95076a28e1
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-21 23:05:48 +0000

    add task 3 files

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+namespace Enemies
+{
+    ///<summary>Represents Zombie class</summary>
+    public class Zombie
+    {
+        ///<summary>Sets health value</summary>
+        private int health;
+        ///<summary>Sets heal to zero</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        ///<summary>Sets health above zero</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            this.health = value;
+        }
+        ///<summary>Returns value of health</summary>
+        public int GetHealth()
+        {
+            return this.health;
+        }
+    }
+}
