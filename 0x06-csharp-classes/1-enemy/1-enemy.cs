c91a3f541f80e734f93f6a876bc3ef88d930ae05
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-21 21:05:05 +0000

    add task 1 files

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>Represents Zombie class</summary>
+    public class Zombie
+    {
+        ///<summary>Sets health value</summary>
+        public int health;
+        ///<summary>Sets heal to zero</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+    }
+}
