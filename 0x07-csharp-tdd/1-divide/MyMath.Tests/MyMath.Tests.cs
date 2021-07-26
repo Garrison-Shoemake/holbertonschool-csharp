7ad8526eb2fe9aa6908b060cf3f7f46d4a776c4f
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-26 23:57:12 +0000

    typo fixes

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -14,9 +14,9 @@ namespace Tests
         }
         public void TestRegularDivision()
         {
-            int[,] matrix = {{2, 4}, {6,8}, {10,12}}
+            int[,] matrix = {{2, 4}, {6,8}, {10,12}};
             int[,] result = matrix.Divide(matrix, 2);
-            Assert.AreEqual(result, new int[,] {{1,2}. {3,4}, {5,6}});
+            Assert.AreEqual(result, new int[,] {{1,2}, {3,4}, {5,6}});
         }
     }
 }
