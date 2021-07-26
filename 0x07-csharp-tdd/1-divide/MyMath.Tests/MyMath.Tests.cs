4a037a88fc0991637778c6f44263a607c5673b9c
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-26 23:56:19 +0000

    added regular divide and null check tests

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -5,12 +5,18 @@ namespace Tests
     [TestFixture]
     public class Tests
     {
-
-
         [Test]
-        public void Test1()
-        {
 
+        public void TestNullCheck()
+        {
+            result = Matrix.Divide(null, 5)
+            Assert.IsNull(result);
+        }
+        public void TestRegularDivision()
+        {
+            int[,] matrix = {{2, 4}, {6,8}, {10,12}}
+            int[,] result = matrix.Divide(matrix, 2);
+            Assert.AreEqual(result, new int[,] {{1,2}. {3,4}, {5,6}});
         }
     }
 }
