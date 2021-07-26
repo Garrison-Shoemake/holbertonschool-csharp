a6e136819c8db3221752bd3c45ee461f460a226f
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-26 23:16:58 +0000

    use getLength instead of length

--- 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -16,9 +16,9 @@ namespace MyMath
                     System.Console.WriteLine("Num cannot be 0");
                     return null;
                 }
-                while (i < matrix.Length)
+                while (i < matrix.GetLength(0))
                 {
-                    while (j < matrix.Length)
+                    while (j < matrix.GetLength(1))
                     {
                         matrix[i,j] /= num;
                         j++;
