d302e1755ecbe1fcd56d531ebb84ac5e47caa217
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-27 20:51:17 +0000

    add task 1 files

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<summary>Class object</summary>
+class Obj
+{
+    ///<summary>Finds if an object is an instance of an array</summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (obj is Array)
+        {
+            return true;
+        }
+        return false;
+    }
+}
+
