63e3ef49496f7acea299b911e5dc5f06a7e19821
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-27 00:38:11 +0000

    add new tests

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -1,6 +1,6 @@
 using NUnit.Framework;
 
-namespace Tests
+namespace MyMath.Tests
 {
     [TestFixture]
     public class Tests
@@ -8,9 +8,16 @@ namespace Tests
         [Test]
         public void TestRegularDivision()
         {
-            int[,] matrix = {{2, 4}, {6,8}, {10,12}};
-            int[,] result = Matrix.Divide(matrix, 2);
-            Assert.AreEqual(result, new int[,] {{1,2}, {3,4}, {5,6}});
+            int[,] test = {{2, 4}, {6,8}, {10,12}};
+            int[,] result = Matrix.Divide(test, 2);
+            Assert.AreEqual(result, new int[,]{{1,2}, {3,4}, {5,6}});
+        }
+        [Test]
+        public void TestNullDivision()
+        {
+            int[,] test = null;
+            int[,] result = Matrix.Divide(test, 3);
+            Assert.AreEqual(result, null);
         }
     }
 }
