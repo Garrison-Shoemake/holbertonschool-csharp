999b7e8ea610b0b0637a4362195b04ba1f31f930
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-27 00:16:13 +0000

    capital M issue?

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -9,7 +9,7 @@ namespace Tests
         public void TestRegularDivision()
         {
             int[,] matrix = {{2, 4}, {6,8}, {10,12}};
-            int[,] result = matrix.Divide(matrix, 2);
+            int[,] result = Matrix.Divide(matrix, 2);
             Assert.AreEqual(result, new int[,] {{1,2}, {3,4}, {5,6}});
         }
     }
