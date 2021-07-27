b00d4758be775b4eb76e45d13a95a7c364d074b0
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-27 03:07:37 +0000

    edit for int return

--- 0x07-csharp-tdd/4-unique/Text/Text.cs
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -8,16 +8,16 @@ namespace Text
         ///<summary>Checks for first unique character</summary>
         public static int UniqueChar(string s)
         {
-            char unique = new char();
+            int index = 0;
             for (int i = 0; i < s.Length; i++)
             {
                 if (s.IndexOf(s[i], i + 1) == -1)
                 {
-                    unique = s[i];
+                    index = i;
                     break;
                 }
             }
-            return unique;
+            return index;
         }
     }
 }
