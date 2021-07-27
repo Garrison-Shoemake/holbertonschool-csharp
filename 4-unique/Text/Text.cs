fe45cc957d7be5620a151881a2883c6cdf8b42d5
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-27 02:58:04 +0000

    add XML doc

--- 4-unique/Text/Text.cs
+++ 4-unique/Text/Text.cs
@@ -2,11 +2,13 @@
 
 namespace Text
 {
+    ///<summary>Class for string methods</summary>
     public class Str
     {
+        ///<summary>Checks for first unique character</summary>
         public static int UniqueChar(string s)
         {
-            char unique = "";
+            char unique = new char();
             for (int i = 0; i < s.Length; i++)
             {
                 if (s.IndexOf(s[i], i + 1) == -1)
