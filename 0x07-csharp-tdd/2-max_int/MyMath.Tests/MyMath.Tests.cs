bf77c83023fcab8f2fa4906487ec48f6a3364c37
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-27 01:10:16 +0000

    add unit tests

--- 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -1,18 +1,44 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 
-namespace Tests
+namespace MyMath.Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void Test1()
+        public void MaxAtEnd()
         {
-            Assert.Pass();
+            List<int> test = new List<int>() {0, 1, 2, 100};
+            int result = Operations.Max(test);
+            Assert.AreEqual(result, 100);
+        }
+        [Test]
+        public void MaxAtBeginning()
+        {
+            List<int> test = new List<int>() {100, 1, 2, 0};
+            int result = Operations.Max(test);
+            Assert.AreEqual(result, 100);
+        }
+        [Test]
+        public void MaxInMiddle()
+        {
+            List<int> test = new List<int>() {0, 100, 1, 2};
+            int result = Operations.Max(test);
+            Assert.AreEqual(result, 100);
+        }
+        [Test]
+        public void MaxWithNegative()
+        {
+            List<int> test = new List<int>() {0, 100, -1, -2};
+            int result = Operations.Max(test);
+            Assert.AreEqual(result, 100);
+        }
+        [Test]
+        public void EmptyList()
+        {
+            List<int> test = new List<int>() {};
+            int result = Operations.Max(test);
+            Assert.AreEqual(result, 0);
         }
     }
 }
