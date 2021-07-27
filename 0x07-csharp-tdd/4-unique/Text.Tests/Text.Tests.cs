d9bffac419e19dc6258351ff2ee2b2a8be39fcac
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-27 03:32:36 +0000

    add tests

--- 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -7,8 +7,39 @@ namespace Text.Tests
     {
 
         [Test]
-        public void Test1()
+        public void FirstChar()
         {
+            string test = "abbccdd";
+            int result = Str.UniqueChar(test);
+            Assert.AreEqual(result, 0);
+        }
+        [Test]
+        public void LastChar()
+        {
+            string test = "bbccdda";
+            int result = Str.UniqueChar(test);
+            Assert.AreEqual(result, 6);
+        }
+        [Test]
+        public void MiddleChar()
+        {
+            string test = "bbaccdd";
+            int result = Str.UniqueChar(test);
+            Assert.AreEqual(result, 2);
+        }
+        [Test]
+        public void NoUniques()
+        {
+            string test = "bbaaccdd";
+            int result = Str.UniqueChar(test);
+            Assert.AreEqual(result, -1);
+        }
+        [Test]
+        public void EmptyString()
+        {
+            string test = "";
+            int result = Str.UniqueChar(test);
+            Assert.AreEqual(result, -1);
         }
     }
 }
