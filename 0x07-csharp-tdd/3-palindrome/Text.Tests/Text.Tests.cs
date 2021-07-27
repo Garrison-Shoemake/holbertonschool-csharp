2c25229e1850f8e41f79b99071ffdc2dc5d2d70f
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-27 03:56:49 +0000

    add tests

--- 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -5,9 +5,28 @@ namespace Text.Tests
     public class Tests
     {
         [Test]
-        public void Test1()
+        public void RegularPalindrome()
         {
-
+            string test = "abcdedcba";
+            Assert.IsTrue(Str.IsPalindrome(test));
+        }
+        [Test]
+        public void CaseTest()
+        {
+            string test = "aBcDedCbA";
+            Assert.IsTrue(Str.IsPalindrome(test));
+        }
+        [Test]
+        public void PunctSpaceTest()
+        {
+            string test = "a, b, c, d, e, d, c, b, a";
+            Assert.IsTrue(Str.IsPalindrome(test));
+        }
+        [Test]
+        public void EmptyString()
+        {
+            string test = "";
+            Assert.IsTrue(Str.IsPalindrome(test));
         }
     }
 }
