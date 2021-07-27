cc2a3c884abe6419d77125ac5b6000fa9a9b37a5
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-27 21:29:51 +0000

    add task 2 files

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+///<summary>Class object</summary>
+class Obj
+{
+    ///<summary>Checks if a class is a subclass</summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.IsSubclassOf(baseType) == true) return true;
+        return false;
+    }
+}
+
