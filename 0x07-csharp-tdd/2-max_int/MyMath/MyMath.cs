fd8c29ebfddd57b15bf3bfeec73370f24758e093
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-27 00:57:29 +0000

    add files for task 2

---
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyMath
+{
+    public class Operations
+    {
+
+        public static int Max(List<int> nums)
+        {
+            if (myList == null || myList.Count == 0)
+            {
+                return 0;
+            }
+            myList.Sort();
+            return myList[myList.Count - 1];
+        }
+    }
+}
