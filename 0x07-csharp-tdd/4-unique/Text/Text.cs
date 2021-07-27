148161bc2869635195882c6fe188473e22be1d92
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-27 03:12:49 +0000

    XML fix

--- 0x07-csharp-tdd/4-unique/Text/Text.cs
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -8,7 +8,6 @@ namespace Text
         ///<summary>Checks for first unique character</summary>
         public static int UniqueChar(string s)
         {
-            int index = 0;
             for (int i = 0; i < s.Length; i++)
             {
                 if (s.IndexOf(s[i], i + 1) == -1)
