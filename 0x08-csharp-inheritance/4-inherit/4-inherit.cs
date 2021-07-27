b66c829610103efa7ce1dc13360671ad20a81ad1
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-27 22:25:22 +0000

    add task 4 files

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+///<summary>Public class Animal</summary>
+public class Animal
+{
+
+}
+///<summary>Public class Dog, deriven from Animal</summary>
+public class Dog : Animal
+{
+
+}
