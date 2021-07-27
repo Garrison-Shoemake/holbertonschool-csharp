07eea276999406d58e7e5e61eda65a9e3ca18ab3
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-27 04:17:34 +0000

    add task 5 files

---
+++ 0x07-csharp-tdd/5-camelcase/Text/Text.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Text
+{
+///<summary>Class for string functions</summary>
+    public class Str
+    {
+        ///<summary>Finds how many words are in a string</summary>
+        public static int CamelCase(string s)
+        {
+            int count = 0;
+            int index = 0;
+
+            try
+            {
+                while (index < s.Length)
+                {
+                    if (char.IsUpper(s[index]) == true)
+                    {
+                        count++;
+                    }
+                    index++;
+                }
+                return count;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+}
