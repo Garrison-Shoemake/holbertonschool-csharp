3fe8a2e5d923b8c5a1358f75e1e5fac4752f4f03
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-27 04:41:56 +0000

    add tests

--- 0x07-csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
@@ -6,9 +6,32 @@ namespace Text.Tests
     {
 
         [Test]
-        public void Test1()
+        public void OneWord()
         {
- 
+            string test = "word";
+            int result = Str.CamelCase(test);
+            Assert.AreEqual(result, 1);
+        }
+        [Test]
+        public void TwWord()
+        {
+            string test = "wordWord";
+            int result = Str.CamelCase(test);
+            Assert.AreEqual(result, 2);
+        }
+        [Test]
+        public void ManyWord()
+        {
+            string test = "wordWordWord";
+            int result = Str.CamelCase(test);
+            Assert.AreEqual(result, 3);
+        }
+        [Test]
+        public void NoWord()
+        {
+            string test = "";
+            int result = Str.CamelCase(test);
+            Assert.AreEqual(result, 0);
         }
     }
 }
