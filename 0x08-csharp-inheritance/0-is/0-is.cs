bd0f019f2573b2e6cb2aa0cbe59c2e38abc961e3
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-27 20:41:53 +0000

    add task 0 files

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+///<summary>Object class</summary>
+class Obj
+{
+    ///<summary>Finds if an object is of type int</summary>
+    public static bool IsOfTypeInt(object obj)
+    {
+        if (obj is int)
+        {
+            return true;
+        }
+        return false;
+    }
+}
