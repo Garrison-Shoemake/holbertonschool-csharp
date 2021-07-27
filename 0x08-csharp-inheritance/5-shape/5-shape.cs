e372d660868acf8b1a42baf94b97767e800a5a74
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-27 22:36:47 +0000

    add task 5 files

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+///<summary>Base Class</summary>
+public class Shape
+{
+    ///<summary>Throws exception</summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
