9d0081c8d4c3957aa5e013b914d00d0dff5368b0
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-27 01:33:56 +0000

    add task 3 files

---
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Text
+{
+    ///<summary>Class for string methods</summary>
+    public class Str
+    {
+        ///<summary>Checks a string if it is a palindrome</summary>
+        public static bool IsPalindrome(string s)
+        {
+            string rev;
+            char[] ch = s.ToCharArray();
+            Array.Reverse(ch);
+            rev = new string(ch);
+            bool b = s.Equals(rev, StringComparison.OrdinalIgnoreCase);
+         if (b == true) 
+         {
+            return true;
+         } 
+         else 
+         {
+            return false;
+         }
+        }
+    }
+}
