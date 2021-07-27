97e46be712e92d009374baef8ce6b1412ae3c756
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-27 02:17:18 +0000

    add task 4 files

---
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Text
+{
+    ///<summary>String class has standard string functions</summary>
+    public class Str
+    {
+        ///<summary>checks for the first non-repeating character in a string</summary>
+        public static int UniqueChar(string s)
+        {
+            char unique = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s.IndexOf(s[i], i+1) == -1)
+                {
+                    unique = s[i];
+                    break;
+                }
+            }
+            return unique;
+        }
+    }
+}
