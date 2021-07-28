f98f6111609dde45ad0d6985cfd26dc1c3c89ec7
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-28 00:15:34 +0000

    add task 9 files

---
+++ 0x08-csharp-inheritance/9-shape/9-shape.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+///<summary>Base Class</summary>
+public class Shape
+{
+    ///<summary>Throws exception</summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+///<summary>REctangle class inherits from Shape</summary>
+public class Rectangle : Shape
+    {
+        private int width;
+        private int height;
+        ///<summary>Gets and sets width</summary>
+        public int Width
+        {
+            get => width;
+            set 
+            {
+                if (value < 0) throw new ArgumentException("Width must be greater than or equal to 0");
+                else width = value;
+            }
+        }
+        ///<summary>Gets and sets Height</summary>
+        public int Height
+        {
+            get => height;
+            set
+            {
+                if (value < 0) throw new ArgumentException("Height must be greater than or equal to 0");
+                else height = value;
+            }
+        }
+        ///<summary>Returns the area of the rectangle</summary>
+        public new int Area()
+        {
+            return height * width;
+        }
+        ///<summary>Overrides ToString for Rectangle</summary>
+        public override string ToString() => $"[Rectangle] {this.width} / {this.height}";
+
+    }
+///<summary>Square class inherits from Rectangle</summary>
+public class Square : Rectangle
+{
+    private int size;
+    ///<summary>Size of the Square</summary>
+    public int Size
+    {
+        get => size;
+        set
+        {
+            if (value < 0) throw new ArgumentException("Size must be greater than or equal to 0");
+            else
+            {
+                size = value;
+                Width = value;
+                Height = value;
+            }
+        }
+    }
+    ///<summary>Overrides ToString for Square</summary>
+    public override string ToString() => $"[Square] {this.size} / {this.size}";
+}
