d050122597dce53fca3b83492fa4a7369c65f85c
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-07-28 01:24:17 +0000

    add task 3 files

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+///<summary>Class object</summary>
+class Obj
+{
+    ///<summary>Prints names of available properties</summary>
+    public static void Print(object myObj)
+    {
+        
+        PropertyInfo[] properties = myObj.GetType().GetProperties();
+        MethodInfo[] methods = myObj.GetType().GetMethods();
+
+        System.Console.WriteLine(myObj.GetType().Name + " Properties:");
+        foreach (PropertyInfo property in properties)
+        {
+            System.Console.WriteLine(property.Name);
+        }
+        System.Console.WriteLine(myObj.GetType().Name + " Methods:");
+        foreach (MethodInfo method in methods)
+        {
+            System.Console.WriteLine(method.Name);
+        }
+    
+    }
+}
+
