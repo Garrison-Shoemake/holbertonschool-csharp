78dbb1641bd0db6b6557ffb457bbab90dba42de7
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-08-03 04:57:41 +0000

    edit -1 check

--- 0x00-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x00-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -10,6 +10,10 @@
             {
                 return wrong;
             }
+            if (matrix.GetLength(1) < 2 || matrix.GetLength(1) > 3)
+            {
+                return wrong;
+            }
             
 
             for (int i = 0; i < matrix.GetLength(0); i++)
