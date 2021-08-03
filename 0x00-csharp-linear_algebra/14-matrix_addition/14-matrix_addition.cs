eac7e125dca64746e2cb9745d7e376f2c803c54d
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-08-03 04:35:27 +0000

    reset task

--- 0x00-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x00-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -6,10 +6,6 @@
         public static double[,] Add(double[,] matrix1, double[,] matrix2)
         {
             double[,] ret = new double[matrix1.GetLength(0),matrix1.GetLength(1)];
-            if ( (matrix1.GetLength(0) != matrix2.GetLength(0)) || (matrix1.GetLength(0) < 2 || matrix1.GetLength(0) > 3) || (matrix2.GetLength(0) < 2 || matrix2.GetLength(0) > 3) )
-            {
-                return ret = new double[1]{-1};
-            }
             for (int i = 0; i < matrix1.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix1.GetLength(1); j++)
