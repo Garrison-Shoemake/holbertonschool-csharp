bf06bddc10e954e3e1289cd2f68a8f1b3185bd11
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-08-03 04:55:36 +0000

    edit -1 check

--- 0x00-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x00-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -6,12 +6,10 @@
         {
             double[,] wrong = new double[,]{{-1}};
             double[,] ret = new double[matrix.GetLength(0),matrix.GetLength(1)];
-            if (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3)
+            if ((matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2) || (matrix.GetLength(0) != 3 || matrix.GetLength(1) != 3))
             {
                 return wrong;
             }
-            
-
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
