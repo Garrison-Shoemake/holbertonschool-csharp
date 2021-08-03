7f71094b5b8107d782a30b771cfc0b46cc2fecce
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-08-03 03:50:07 +0000

    negative one return

--- 0x00-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
+++ 0x00-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -6,6 +6,10 @@ class VectorMath
     public static double[] Multiply(double[] vector, double scalar)
     {
         double[] ret = new double[vector.Length];
+        if (vector.Length < 2 || vector.Length > 3)
+        {
+            return ret = new double[1]{-1};
+        }
         for (int i = 0; i < vector.Length; i++)
         {
             ret[i] = (vector[i] * scalar);
