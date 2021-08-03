1f1ea30c5fa283d98deea8503f552dda090a18f4
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-08-03 04:52:10 +0000

    add task 16 files

---
+++ 0x00-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+    class MatrixMath
+    {
+        public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+        {
+            double[,] wrong = new double[,]{{-1}};
+            double[,] ret = new double[matrix.GetLength(0),matrix.GetLength(1)];
+            if (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3)
+            {
+                return wrong;
+            }
+            
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    ret[i,j] = matrix[i,j] * scalar;
+                }
+            }
+            return ret;
+        }
+    }
+
