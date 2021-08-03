be0f00f3da262b61b991a74d0632baa28d9b5e6d
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-08-03 04:42:05 +0000

    add more -1 checks

--- 0x00-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x00-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -5,7 +5,20 @@
     {
         public static double[,] Add(double[,] matrix1, double[,] matrix2)
         {
+            double[,] wrong = new double[,]{{-1}};
             double[,] ret = new double[matrix1.GetLength(0),matrix1.GetLength(1)];
+            if (matrix1.GetLength(0) < 2 || matrix1.GetLength(0) > 3)
+            {
+                return wrong;
+            }
+            if (matrix2.GetLength(0) < 2 || matrix2.GetLength(0) > 3)
+            {
+                return wrong;
+            }
+            if (matrix1.GetLength(0) != matrix2.GetLength(0))
+            {
+                return wrong;
+            }
             for (int i = 0; i < matrix1.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix1.GetLength(1); j++)
