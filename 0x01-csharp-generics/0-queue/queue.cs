3de9f6c00dffbe967af66fb45475386812fd1a51
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-08-03 21:26:31 +0000

    add task 0 files

---
+++ 0x01-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+///<summary>New generic class Queue, emulating an actual Queue class</summary>
+public class Queue<T>
+{
+    ///<summary>Returns the type of the Queue given</summary>
+        public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+}
+
