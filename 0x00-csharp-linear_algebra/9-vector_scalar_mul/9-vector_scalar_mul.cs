9c3ae99dc3e69d747c40764c1b366fa7528cab52
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-08-03 03:41:19 +0000

    added files in right directory

---
+++ 0x00-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+class VectorMath
+{
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        double[] ret = new double[vector.Length];
+        for (int i = 0; i < vector.Length; i++)
+        {
+            ret[i] = (vector[i] * scalar);
+        }
+        return ret;
+    }
+}
+
