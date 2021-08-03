7cb9b8e844933f6f2eec4fa96312390829c54d49
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-08-03 01:57:53 +0000

    add task 3 files

---
+++ 0x00-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        int i = 0;
+        double sum = 0;
+
+        if (vector.Length < 2 && vector.Length > 3)
+        {
+            return -1;
+        }
+        for (; i < vector.Length; i++)
+        {
+            sum += Math.Pow(vector[i], 2);    
+        }
+        return Math.Round(Math.Sqrt(sum), 2);
+    }
+}
+
