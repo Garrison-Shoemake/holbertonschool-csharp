76165b6353c132a1a5aa92b7aae7105752338ac6
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-08-04 23:46:07 +0000

    add task 2 files

---
+++ 0x02-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+///<summary>Base abstract class</summary>
+abstract class Base
+{
+    public string name = "";
+
+    public override string ToString() => $"{this.name} is a {this.GetType()}";
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+interface IBreakable
+{
+    int durability {get; set;}
+    void Break();
+}
+interface ICollectable
+{
+    bool isCollected {get; set;}
+    void Collect();
+}
+class Door : Base, IInteractive
+{
+    public Door(string name="Door")
+    {
+        this.name = name;
+    }
+    public void Interact()
+    {
+        System.Console.WriteLine($"You try to open the {this.name}. It's locked.");
+        return;
+    }
+}
