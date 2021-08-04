a719e740be37059dcee2279b0acefa130e9cc7db
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-08-04 22:12:48 +0000

    add task 0 files

---
+++ 0x02-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+///<summary>Base abstract class</summary>
+abstract class Base
+{
+    public string name = "";
+
+    public override string ToString() => $"{this.name} is a {this.GetType()}";
+}
