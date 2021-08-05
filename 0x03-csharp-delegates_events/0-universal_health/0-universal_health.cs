f8784f7c954b372480ef3716e3134620a5e32baf
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-08-05 23:11:27 +0000

    add negative checks

--- 0x03-csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ 0x03-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -10,10 +10,17 @@ public class Player
     public Player(string name="Player", float maxHp=100f)
     {
         this.name = name;
-        this.maxHp = maxHp;
-        this.hp = maxHp;
         if (maxHp < 0)
+        {
             System.Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");   
+            this.maxHp = 100f;
+            this.hp = 100f;
+        }
+        else
+        {
+            this.maxHp = maxHp;
+            this.hp = maxHp;
+        }
     }
     ///<summary>Prints the players health and max health</summary>
     public void PrintHealth() {System.Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");}
