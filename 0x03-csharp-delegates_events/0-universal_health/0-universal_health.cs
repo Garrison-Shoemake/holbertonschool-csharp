456b3f3f7df17f35fa72cb9a2352105121870aa7
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-08-05 23:08:09 +0000

    add task 0 files

---
+++ 0x03-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,20 @@
+﻿using System;
+///<summary>Player class</summary>
+public class Player
+{
+    string name;
+    float maxHp;
+    float hp;
+
+///<summary>Player constructor.</summary>
+    public Player(string name="Player", float maxHp=100f)
+    {
+        this.name = name;
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+        if (maxHp < 0)
+            System.Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");   
+    }
+    ///<summary>Prints the players health and max health</summary>
+    public void PrintHealth() {System.Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");}
+}
