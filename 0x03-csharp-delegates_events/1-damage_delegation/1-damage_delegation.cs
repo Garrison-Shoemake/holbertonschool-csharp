e9f1027374807c693d1f2616e49e0f7eaa29e681
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-08-06 00:15:30 +0000

    add task 1 files

---
+++ 0x03-csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+///<summary>Player class</summary>
+public class Player
+{
+    string name;
+    float maxHp;
+    float hp;
+
+///<summary>Player constructor.</summary>
+    public Player(string name="Player", float maxHp=100f)
+    {
+        this.name = name;
+        if (maxHp < 0)
+        {
+            System.Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");   
+            this.maxHp = 100f;
+            this.hp = 100f;
+        }
+        else
+        {
+            this.maxHp = maxHp;
+            this.hp = maxHp;
+        }
+    }
+    ///<summary>Prints the players health and max health</summary>
+    public void PrintHealth() {System.Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");}
+
+    delegate void CalculateHealth(float amount);
+    ///<summary>Method for taking damage</summary>
+    public void TakeDamage(float damage)
+    {
+        
+        if (damage < 0)
+            System.Console.WriteLine($"{this.name} takes 0 damage!");   
+        else
+            System.Console.WriteLine($"{this.name} takes {damage} damage!");
+    }
+    ///<summary>Method for healing damage</summary>
+    public void HealDamage(float heal)
+    {
+        if (heal < 0) System.Console.WriteLine($"{this.name} heals 0 HP!");
+        else System.Console.WriteLine($"{this.name} heals {heal} HP!");
+
+    }
+
+
+}
+
